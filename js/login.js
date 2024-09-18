d0d801ecbc03d7b4c310db34043d67f879931fd8
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2024-09-19 01:28:07 +0600

    home section setup done

--- js/login.js
+++ js/login.js
@@ -1,12 +1,26 @@
 
 //step-1 :set even handler
 document.getElementById('btn-login').addEventListener('click',function(event){
+
     // step-2 :prevent default behavior (prevent reloading browser)
-    event.preventDefault(); // 
+    event.preventDefault(); 
     console.log('login button click');
-    //step-3: get the phone number
+
+    //step-3: get the phone number and the pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
     
-    
+    //step-4: validate phone number & pin
+    //this is temporary. You should not do like this
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        //step-5: Allow user to use the website
+        console.log('You are login');
+        window.location.href= '/home.html'
+        
+    }
+    else{
+        alert('Wrong mobile number or Pin');
+        
+    }
 })
